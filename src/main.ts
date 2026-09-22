72cd5e534607117320ec0966fa6554a663ba6982
Максим <maksim@MacBook-Air-Maksim.local>
2026-09-22 14:31:45 +0300

    added design rules in variables

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ import "@ionic/vue/css/display.css";
 // import "@ionic/vue/css/palettes/dark.system.css";
 
 /* Theme variables */
-import "./theme/variables.css";
+import "./theme/variables.scss";
 import {subscriptionStore} from "@/stores/subscriptionStore";
 
 const pinia = createPinia();
